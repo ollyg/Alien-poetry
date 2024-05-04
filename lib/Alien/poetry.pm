458c0a9df2c73f823290262391061d9b1acb4b76
Oliver Gorwits <oliver@cpan.org>
2024-05-04 13:46:03 +0100

    update bin_dir to get sharedir prefix where installed

--- lib/Alien/poetry.pm
+++ lib/Alien/poetry.pm
@@ -4,11 +4,11 @@ use strict;
 use warnings;
 use parent qw( Alien::Base );
 
+use Path::Tiny;
+use File::ShareDir::Dist qw( dist_share );
+
 our $VERSION = '1.0108020000';
 
-sub bin_dir {
-  my($self) = @_;
-  $self->runtime_prop->{bin_dir},
-};
+sub bin_dir { path( dist_share __PACKAGE__ )->child('bin') };
 
 1;
