7725ac5ee997a6622e9e9cbb3d95a6f258da2d37
Oliver Gorwits <oliver@cpan.org>
2024-05-04 18:19:27 +0100

    always rebuild/install and add poetry binary function

--- lib/Alien/poetry.pm
+++ lib/Alien/poetry.pm
@@ -7,8 +7,12 @@ use parent qw( Alien::Base );
 use Path::Tiny;
 use File::ShareDir::Dist qw( dist_share );
 
-our $VERSION = '1.0108020000';
+# for now, pull poetry from python-poetry.org
+# would bump to v2 and include poetry --version in this if we switch to bundle
+our $VERSION = '1.000000';
 
-sub bin_dir { path( dist_share __PACKAGE__ )->child('bin') };
+sub bin_dir { path( dist_share __PACKAGE__ )->child('venv')->child('bin') }
+
+sub poetry { path( (shift)->bin_dir )->child('poetry') }
 
 1;
