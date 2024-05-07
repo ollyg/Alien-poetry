c2f80a9a112602014997d0d23fbc65a7b4d77bc2
Oliver Gorwits <oliver@cpan.org>
2024-05-07 14:02:32 +0100

    fix issues with poetry not being relocateable

--- lib/Alien/poetry.pm
+++ lib/Alien/poetry.pm
@@ -12,7 +12,6 @@ use File::ShareDir::Dist qw( dist_share );
 our $VERSION = '1.000000';
 
 sub bin_dir { path( dist_share __PACKAGE__ )->child('venv')->child('bin') }
-
 sub poetry { path( (shift)->bin_dir )->child('poetry') }
 
 1;
