293106beab1000888b71d58a451873dc85fb1681
Oliver Gorwits <oliver@cpan.org>
2024-05-12 10:04:21 +0100

    remove Eric Miller ref in amended BSD Lic

--- lib/Alien/poetry.pm
+++ lib/Alien/poetry.pm
@@ -75,7 +75,7 @@ notice, this list of conditions and the following disclaimer.
 notice, this list of conditions and the following disclaimer in the
 documentation and/or other materials provided with the distribution.
 
-* Neither the name of Eric A. Miller's Organization
+* Neither the name of the copyright holder
 nor the names of its contributors may be used to endorse or promote
 products derived from this software without specific prior written
 permission.
